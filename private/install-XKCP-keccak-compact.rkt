e0e938e5e2da848d911d23122d63ab25b1d64ecd
bkc39 <pl.proofs@gmail.com>
2025-07-23 21:44:49 -0400

    remove wget from build step

--- private/install-XKCP-keccak-compact.rkt
+++ private/install-XKCP-keccak-compact.rkt
@@ -8,11 +8,6 @@
  racket/system
  setup/dirs)
 
-(define source-repo
-  "https://raw.githubusercontent.com/bkc39/XKCP/master")
-(define keccak-compact-src-path
-  "Standalone/CompactFIPS202/C/Keccak-more-compact.c")
-
 (define (delete-if-exists path)
   (when (file-exists? path)
     (delete-file path)))
@@ -22,9 +17,6 @@
     (build-path this-collection-path "private"))
   (define keccak-source-path
     (build-path private-path "Keccak-more-compact.c"))
-  (define wget
-    (or (find-executable-path "wget")
-        (error 'pre-installer "wget executable not found")))
   (define lib-dir
     (if user-specific? (find-user-lib-dir) (find-lib-dir)))
   (define shared-object-target
@@ -33,10 +25,8 @@
   (make-directory* lib-dir)
   (delete-if-exists shared-object-target)
   (parameterize ([current-directory private-path])
-    (delete-if-exists keccak-source-path)
-    (system* wget (string-append source-repo "/" keccak-compact-src-path))
     (unless (file-exists? keccak-source-path)
-      (error 'pre-installer "wget did not download keccak source file"))
+      (error 'pre-installer "couldn't locate keccak source file"))
 
     (parameterize ([current-extension-linker-flags
                     (append
